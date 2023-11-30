a898ba2c2b8d9e27594ac59cb779914d6f970d29
nickthecook <nickthecook@gmail.com>
2023-11-30 09:11:29 -0500

    release 2.3.1

--- Formula/ops.rb
+++ Formula/ops.rb
@@ -2,8 +2,8 @@
 class Ops < Formula
   desc "Lightweight automation for your project"
   homepage "https://github.com/nickthecook/crops"
-  url "https://github.com/nickthecook/crops/archive/refs/tags/2.3.0.tar.gz"
-  sha256 "76ffc1be1739aca690a8225881641e47e07ce5560d0a92c58c1102a9cbda0595"
+  url "https://github.com/nickthecook/crops/archive/refs/tags/2.3.1.tar.gz"
+  sha256 "8d1bfee90632c7a52b2b72f0eb3f8abeac94769a55c5b326e5ef27a8a2b59b49"
   license "GPL-3.0-only"
 
   depends_on "crystal" => :build
